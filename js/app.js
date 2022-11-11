92d1756976b17c1f157b47d6d88250b54543bfec
Thunderbird93 <antircristian@gmail.com>
2022-11-11 16:10:10 +0100

    ex

--- js/app.js
+++ js/app.js
@@ -2,6 +2,28 @@ const { createApp } = Vue;
 
 createApp({
     data(){
-        return{}
+        return{
+            title: 'Benvenuto Ajax',
+            numero: ''
+        }
+    },
+
+    methods:{
+        getApi(){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(risultato => {
+                console.log('Fine chiamata axios', risultato.data);
+                this.numero=risultato.data.response
+            })
+            .catch(erore => {
+                thisa.title = errore.code
+                console.log(errore.code);
+            })
+        }
+    },
+
+    mounted(){
+        this.getApi();
+        console.log('Log Axios');
     }
 }).mount("#app");
